4ce94b5b1e15bb78a2ba85ce89e59cc0832d46e3
vcolluto <86895240+vcolluto@users.noreply.github.com>
2023-04-07 12:18:02 +0200

    creata classe e test chiamata metodi sul program

--- csharp-geometria/Program.cs
+++ csharp-geometria/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using csharp_geometria;
+
+int b, h;
+
+Console.Write("Inserisci la base del rettangolo: ");
+b=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Inserisci l'altezza del rettangolo: ");
+h = Convert.ToInt32(Console.ReadLine());
+
+Rettangolo r1=new Rettangolo(b, h);
+
+Console.WriteLine("Il perimetro è: " + r1.calcolaPerimetro());
+Console.WriteLine("L'area è: " + r1.calcolaArea());
---
+++ csharp-geometria/Rettangolo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_geometria
+{
+    internal class Rettangolo
+    {
+        public int baseRettangolo, altezzaRettangolo;
+
+        public Rettangolo(int baseRettangolo, int altezzaRettangolo)
+        {
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+        }
+
+        public int calcolaArea()
+        {
+            return baseRettangolo * altezzaRettangolo;
+        }
+
+
+        public int calcolaPerimetro()
+        {
+            return 2*(baseRettangolo + altezzaRettangolo);
+        }
+    }
+}
